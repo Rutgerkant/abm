1a940b366720b1aab980d92f38868a0382b1997b
MichielvH04 <M.W.A.vanHouten@student.tudelft.nl>
2024-03-27 11:46:43 +0100

    afg

--- SubsidieModel.py
+++ SubsidieModel.py
@@ -142,11 +142,10 @@ class SubsidieModel(Model):
             self.step()
 
 
-
 class AdoptionAgent(Agent):
     """ Een agent met algemene eigenschappen voor innovatie adoptie."""
-    def __init__(self, unique_id, model, agent_type, belangstelling , subsidie = 0):
-        super().__init__(unique_id, model)
+    def __init__(self, pos, model, agent_type, belangstelling , subsidie = 0):
+        super().__init__(pos, model)
         self.agent_type = agent_type
         self.vermogen = random.normalvariate(mu = 50000, sigma=12500)         #normaalverdeling van het inkomen
         self.belangstelling = belangstelling
@@ -161,37 +160,12 @@ class AdoptionAgent(Agent):
             # Als de agent nog geen EV heeft, voeg dan de subsidie toe aan het vermogen
             if self.subsidie > 0:
                 effectief_vermogen += self.subsidie
-            if self.leeftijd_auto >= auto_leeftijd_drempel and int(effectief_vermogen) >= EV_prijs and self.belangstelling > 0.4:
+            if self.leeftijd_auto >= auto_leeftijd_drempel and effectief_vermogen >= EV_prijs and self.belangstelling > 0.4:
                 self.heeft_ev_gekocht = True   
+
+    
     def step(self):
         # Voorbeeldgedrag: print hun type
+        
         calculate_belangstelling(self)
-        self.proberen_EV_te_kopen(self, )
-        print(f"Agent {self.unique_id} van type {self.agent_type} handelt.")
-
-
-class Innovators(AdoptionAgent):
-    def step(self):
-              #toewijzing van vermogen tussen twee bepaalde intervallen
-        print(f"Innovator {self.unique_id} handelt innovatief.")
-
-class EarlyAdopters(AdoptionAgent):
-    def step(self):
-        
-        print(f"Early Adopter {self.unique_id} adopteert vroeg.")
-
-class EarlyMajority(AdoptionAgent):
-    def step(self):
-        
-        print(f"Early Majority {self.unique_id} overweegt zorgvuldig.")
-
-class LateMajority(AdoptionAgent):
-    def step(self):
-        
-        print(f"Late Majority {self.unique_id} is sceptisch maar volgt uiteindelijk.")
-
-class Laggards(AdoptionAgent):
-    def step(self):
-        
-        print(f"Laggard {self.unique_id} is zeer terughoudend met verandering.")
- 
+        self.proberen_EV_te_kopen
